4e2d47a672ce8ff6c83cb96ad9ffed959a268f7a
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-08-12 14:38:02 -0300

    Renderizando HTML, render, templates, INSTALEED_APPS e TemplateDoesNotExist

--- ola_django/blog/views.py
+++ ola_django/blog/views.py
@@ -1,11 +1,11 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
 
 # Create your views here.
 def blog(request):
     print('blog')
-    return HttpResponse('blog do app 1')
+    return render(request, 'blog/index.html')
 
 def exemplo(request):
     print('exemplo')
-    return HttpResponse('exemplo do app 1')
+    return render(request, 'blog/exemplo.html')
